e2c9bafa097bafcaf46645501e15adb772ba5bfb
Zaid alshibi <zaidealshibi@gmail.com>
2022-10-05 16:18:57 +0300

    added the canDo function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import AddPostForm from "./components/Post/Add-post-form";
 import Post from "./components/Post/Post";
 import './App.css';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Else, If, Then, When } from "react-if";
 import { useAuth } from "./Context/AuthContext";
 import Signin from "./components/Auth/Signin";
 import Signup from "./components/Auth/Signup";
 
 
-function App() {
-  const {isAuth, signup, checkSignIn} = useAuth()
+function App () {
+  const { isAuth, signup, checkSignIn } = useAuth();
   useEffect( () => {
     checkSignIn();
-  } );
+  }, [] );
   return (
     <div className="App">
       <When condition={isAuth}>
-        <AddPostForm/>
-        <Post/>
+        <AddPostForm />
+        <Post />
       </When>
       <When condition={!isAuth}>
         <If condition={signup}>
           <Then>
-            <Signup/>
+            <Signup />
           </Then>
           <Else>
-            <Signin/>
+            <Signin />
           </Else>
         </If>
       </When>
