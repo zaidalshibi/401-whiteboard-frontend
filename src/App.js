bb2430bb3595052301865ff64a33a4e581557332
Zaid alshibi <zaidealshibi@gmail.com>
2022-11-23 03:16:39 +0300

    final commit for redux-02 branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Else, If, Then } from "react-if";
 import AddPostForm from "./components/Post/Add-post-form";
 import Post from "./components/Post/Post";
@@ -6,9 +6,22 @@ import Signin from "./components/Auth/Signin";
 import './App.css';
 import { Button, useColorMode, VStack } from '@chakra-ui/react';
 import { useAuthSelector } from './index';
+import { useEffect } from 'react';
 
 function App () {
   const isAuth = useAuthSelector( state => state.auth.isAuth );
+  const checkAuth = useCallback(() => {
+    if ( isAuth ) {
+      return true
+    } else if ( !isAuth && localStorage.getItem( 'isAuth' ) === 'true' ) {
+      return true
+    } else {
+      return false
+    }
+  }, [ isAuth ] );
+  useEffect( () => {
+    checkAuth();
+  }, [checkAuth, isAuth] );
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <VStack>
@@ -24,7 +37,7 @@ function App () {
       >
         Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
       </Button>
-      <If condition={isAuth}>
+      <If condition={checkAuth()}>
         <Then>
           <AddPostForm />
           <Post />
